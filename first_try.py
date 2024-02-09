e2dd5e8300da89bb0ecc2be38214941ca36daaf1
PetrKochetkov <s22e_kochetkov@179.ru>
2024-02-09 17:03:52 +0300

    новые файлы, а также две различных идеи. Первый -- у каждого уровня будет своя проверка. Второй -- на основе сайта Касперского, будет общий вид пакета данных и по ним будет идти проверка в фаерволле

---
+++ first_try.py
@@ -0,0 +1,33 @@
+class Data(object):
+    Destination_object = int()  # Destination MAC-address, Destination IP-address, Destination Port
+    Source_object = int()  # Source MAC-address, Source IP-address, Source Port
+    flag = str()  # Different flags. For each layer different flags
+    TTL = int()  # Only for Network layer, else = -1
+
+    def __init__(self, destination_address, source_address, flag, ttl=-1):  # -1, так как только у IP есть TTL
+        self.Destination_object = destination_address
+        self.Source_object = source_address
+        self.flag = flag
+        self.TTL = ttl
+
+
+class Frame(Data):  # Link layer, based on MAC protocol
+
+    def __init__(self, destination_mac, source_mac, flag):
+        super().__init__(destination_mac, source_mac, flag)
+
+
+class Packet(Data):  # Internet layer, based on IP
+
+    def __init__(self, destination_ip, source_ip, flag, ttl):
+        super().__init__(destination_ip, source_ip, flag, ttl)
+
+
+class Segment(Data):  # Transport layer, based on TCP
+
+    def __init__(self, destination_port, source_port, flag):
+        super().__init__(destination_port, source_port, flag)
+
+
+class Capsule(object):  # Application layer, based on
+    pass
