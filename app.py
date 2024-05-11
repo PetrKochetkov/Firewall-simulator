7174bc4ed9c90f51b7a9594c1a154a3c983c37cd
PetrKochetkov <s22e_kochetkov@179.ru>
2024-05-11 21:52:08 +0300

    Deletes connection to file in logging

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ def something():
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, filemode="w",
+    logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s %(levelname)s %(message)s")
     conf = get_config()
     firewall_mode = conf['mode']
